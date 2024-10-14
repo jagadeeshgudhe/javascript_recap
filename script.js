ae9fa40ff0f848a9527c83221f045dfbaad82136
Gudhe Jagadeesh <jagadeesh@example.com>
2024-10-14 23:09:54 +0530

    revision

--- script.js
+++ script.js
@@ -1,1 +1,84 @@
-variables
+//variables container that stores  value (variables names should be unique other wise it will through a error)
+
+//declaration let x;
+//assignment x=100;
+/*
+let x;
+x=100;
+console.log(x);
+
+
+//NUMBERS
+let age=25;
+let price=10.99;
+let gpa=2.1;
+console.log(age);
+
+console.log(`you are ${age} years old`);
+console.log(`the price is $ ${price}`);
+console.log(`your gpa is ${gpa}`);            */
+/*
+
+//STRINGSSS->series of characters
+
+let firstname ="Jagadeesh";
+let favouritefood="pizza";
+let email = "jagadeesh8808@gmail.com";
+
+console.log(typeof firstname);
+console.log(firstname);
+console.log(`my name is ${firstname}`);
+console.log(`i like ${favouritefood}`);
+console.log(`my email is ${email}`)   */
+
+/*
+//BOOLEANS -> euther true or false
+
+let online=false;
+let forSale=true;
+let isStudent=true;
+console.log(typeof online);
+console.log(`im in online ${online}`);
+console.log(`i want to sale my mobile ${forSale}`);
+console.log(`enrolled ${isStudent}`);    */
+/*
+
+//Example:
+
+let fullName='Jagadeesh Gudhe';
+let age=25;
+let student=false;
+document.getElementById("p1").textContent=`your name is ${fullName}`;
+document.getElementById("p2").textContent=`your are ${age} years old`;
+document.getElementById("p3").textContent=`enrolled :${student}`    */
+
+//Arithemetic operators 
+
+//let students =31;
+
+//students=students+1;
+//students=students-1;
+//students=students*1;
+//students=students/2;
+//students=students%3; 
+
+//students+=1;
+//students-=1;
+//students*=2;
+//students/=2;
+//students%=2;
+
+//students++;
+//students--;
+//console.log(students);
+
+/*operator precedence order ->
+1.parenthesis
+2.exponents
+3.multiplication, div,modullus
+4.addition , subtraction             */
+
+
+let result=1+2*3+4**2;
+console.log(result);
+
